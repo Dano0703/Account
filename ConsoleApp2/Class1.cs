28c6a189b4b18785c00e5232f52aeec5269226b9
Owner <Owner@DESKTOP-2U3FDTC>
2022-01-27 21:39:41 -0700

    Add project files.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Account
+{
+    internal class Account
+    {
+       private string name; //instance variable
+
+        //metho that sets the account name in the object
+        public void SetName(string accountName)
+        {
+            name = accountName; //stroes the account name
+        }
+
+        //method that retrievs the account name from the object
+        public string GetName()
+        {
+            return name; //returns name's value to this method's caller
+        }
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AccountTest
+{
+     class AccountTest
+    {
+        static void Main()
+        {
+            // Creates an Account object and assign it to myAccount
+            Account myAccount = new Account();
+            //dispaly myAccount's initial name (there isn't one yet)
+            
+            Console.WriteLine($"Initial name is: {myAccount().Getname()}");
+            //prompt for and read the name then put the name in the object
+            
+            Console.Write("Enter Name:");//prompt
+            string theName = Console.ReadLine(); //Reads the name
+            myAccount().SetName(theName); //put theName in the myAccount object
+           
+            //display the name stored in the myAccount object
+            Console.WriteLine($"myAccount's name is: {myAccount().GetName()}");
+
+        }
+    }
+}
